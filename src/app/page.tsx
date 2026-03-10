4f137f421aa511d2f27be1c2b857f4353184417d
unknown <cuch.aleksander@gmail.com>
2026-03-10 12:52:41 +0100

    Lk-3 change fonts

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -18,12 +18,12 @@ export default function Page() {
       <Carousel />
       <div className="w-full bg-black text-white px-6 py-20 ">
         <p className="max-w-5xl mx-auto text-center md:text-xl leading-relaxed tracking-widest font-light text-sm font-trajan">
-          Gdańska Szkoła Fechtunku to miejsce, gdzie pasja do szermierki
-          historycznej łączy się z realnym rozwojem sprawności, koncentracji i
-          charakteru. Oferujemy zajęcia z technik historycznych niezależnie od
-          poziomu zaawansowania. Przyjdź i sprawdź jak zapomniane techniki
-          dawnych mistrzów łączą się z współczesną metodyką sportową. Dołącz do
-          nas i odkryj historię na nowo!
+          GDAŃSKA SZKOŁA FECHTUNKU TO MIEJSCE, GDZIE PASJA DO SZERMIERKI
+          HISTORYCZNEJ ŁĄCZY SIĘ Z REALNYM ROZWOJEM SPRAWNOŚCI, KONCENTRACJI I
+          CHARAKTERU. OFERUJEMY ZAJĘCIA Z TECHNIK HISTORYCZNYCH NIEZALEŻNIE OD
+          POZIOMU ZAAWANSOWANIA. PRZYJDŹ I SPRAWDŹ JAK ZAPOMNIANE TECHNIKI
+          DAWNYCH MISTRZÓW ŁĄCZĄ SIĘ Z WSPÓŁCZESNĄ METODYKĄ SPORTOWĄ. DOŁĄCZ DO
+          NAS I ODKRYJ HISTORIĘ NA NOWO!
         </p>
       </div>
       <BulletSection />
